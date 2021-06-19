f3ee59fc0cce3276d65326095f75902e46025ec9
David <chavezzday@gmail.com>
2021-06-19 12:17:03 -0500

    first section

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
+import { loadImages } from "./load-images";
 import { initParticles } from "./particles";
 
 export const startAll = ()=>{
     initParticles();
+
+    loadImages(); 
 }
---
+++ src/js/load-images.js
@@ -0,0 +1,11 @@
+import menu from '../assets/images/icon-menu.svg'
+
+const loadImages = ()=>{
+
+    document.querySelector('.greeting img').src = menu;
+    
+}
+
+export{
+    loadImages
+}
